ec9d4d4f847c7778b3970c6e77d0e9fef3692b07
pliniooop <pedroservulo980@gmail.com>
2026-03-11 20:10:55 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        double num1, num2, resultado;
+        char operacao;
+
+        System.out.print("Digite o primeiro número: ");
+        num1 = scanner.nextDouble();
+
+        System.out.print("Digite o segundo número: ");
+        num2 = scanner.nextDouble();
+
+        System.out.print("Digite a operação (+, -, *, /): ");
+        operacao = scanner.next().charAt(0);
+
+        if (operacao == '+') {
+            resultado = num1 + num2;
+            System.out.println("Resultado: " + resultado);
+
+        } else if (operacao == '-') {
+            resultado = num1 - num2;
+            System.out.println("Resultado: " + resultado);
+
+        } else if (operacao == '*') {
+            resultado = num1 * num2;
+            System.out.println("Resultado: " + resultado);
+
+        } else if (operacao == '/') {
+
+            if (num2 <= 0) {
+                System.out.println("Impossível dividir!!");
+            } else {
+                resultado = num1 / num2;
+                System.out.println("Resultado: " + resultado);
+            }
+
+        } else {
+            System.out.println("Sinal inválido");
+        }
+
+        scanner.close();
+    }
+}
