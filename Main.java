ecf649c13f057628b1009e0cb1daa1075c8a273c
pliniooop <pedroservulo980@gmail.com>
2026-03-11 19:56:26 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int num1, num2, num3;
+
+        System.out.print("Digite o primeiro número: ");
+        num1 = scanner.nextInt();
+
+        System.out.print("Digite o segundo número: ");
+        num2 = scanner.nextInt();
+
+        System.out.print("Digite o terceiro número: ");
+        num3 = scanner.nextInt();
+
+        if (num1 == num2 && num2 == num3) {
+            System.out.println("Os números são iguais");
+        } else {
+            int maior = num1;
+
+            if (num2 > maior) {
+                maior = num2;
+            }
+
+            if (num3 > maior) {
+                maior = num3;
+            }
+
+            System.out.println("O maior número é: " + maior);
+        }
+
+        scanner.close();
+    }
+}
